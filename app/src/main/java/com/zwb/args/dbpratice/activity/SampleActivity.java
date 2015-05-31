472cafbdecd92c87ab35cbed566d0f9ec03942a7
wenbiao_zheng <wenbiao_zheng@kingdee.com>
2015-05-31 16:42:26 +0800

    增加测试开关按钮

--- app/src/main/java/com/zwb/args/dbpratice/activity/SampleActivity.java
+++ app/src/main/java/com/zwb/args/dbpratice/activity/SampleActivity.java
@@ -5,15 +5,12 @@ import android.support.v7.app.ActionBarActivity;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.Button;
 
 import com.zwb.args.dbpratice.R;
-import com.zwb.args.dbpratice.cache.DatabaseCache;
 import com.zwb.args.dbpratice.event.InsertEvent;
-import com.zwb.args.dbpratice.exception.NoRecordException;
-import com.zwb.args.dbpratice.exception.NoSuchTableException;
 import com.zwb.args.dbpratice.exception.NoTableException;
-import com.zwb.args.dbpratice.exception.NoTagException;
 import com.zwb.args.dbpratice.model.Status;
 import com.zwb.args.dbpratice.util.LogUtil;
 
@@ -22,66 +19,37 @@ import java.util.List;
 
 
 public class SampleActivity extends ActionBarActivity {
-    private TextView tvName;
+    private Button btTest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        List<Status> statuses = new ArrayList<Status>();
-        for (int i = 0; i < 10; i++) {
+        btTest = (Button) findViewById(R.id.bt_test);
+        btTest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addData();
+            }
+        });
+    }
+
+    public void addData() {
+        List<Status> statuses = new ArrayList<>();
+        for (int i = 0; i < 10000; i++) {
             Status status = new Status();
             status.setName("吸金好");
             status.setStatusId("01");
             statuses.add(status);
         }
 
-        Status status = new Status();
-        status.setName("你好");
-        status.setStatusId("02");
-
-        InsertEvent insertEvent1 = new InsertEvent();
         InsertEvent insertEvent = new InsertEvent();
         try {
             insertEvent.to(Status.class).insertAll(statuses);
-            insertEvent1.to(Status.class).insert(status);
         } catch (NoTableException e) {
             LogUtil.e(e.toString());
         }
-
-        DatabaseCache cache = DatabaseCache.getInstance(this);
-        try {
-            cache.insertToDb(Status.class);
-            cache.insertToDb(Status.class);
-        } catch (NoTagException e) {
-            LogUtil.e(e.toString());
-        } catch (NoRecordException e) {
-            LogUtil.e(e.toString());
-        } catch (NoSuchTableException e) {
-            LogUtil.e(e.toString());
-        } catch (NoTableException e) {
-            LogUtil.e(e.toString());
-        }
-        tvName = (TextView) findViewById(R.id.tv_name);
-        List<Status> statusList = null;
-        try {
-            long start = System.currentTimeMillis();
-            statusList = cache.from(Status.class).findAll();
-            long end = System.currentTimeMillis();
-            LogUtil.e("时间:" + (end - start) + ", 提取的长度:" + statusList.size());
-            for (Status s : statusList) {
-                LogUtil.e("id:" + s.getStatusId() + ", name:" + s.getName());
-            }
-        } catch (NoTagException e) {
-            LogUtil.e(e.toString());
-        } catch (NoRecordException e) {
-            LogUtil.e(e.toString());
-        } catch (NoTableException e) {
-            LogUtil.e(e.toString());
-        }
-
-        tvName.setText(statusList.get(0).getName());
     }
 
     @Override
